c90ef589907738901da9007b08c0af734cea1123
David <d.vandevondervoort@gmail.com>
2013-06-26 17:55:09 +0200

    Loading markers to map

--- nodejs/app-prototype.js
+++ nodejs/app-prototype.js
@@ -1,3 +1,7 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/tty.usbmodem411", {
 	baudrate: 38400
@@ -7,18 +11,19 @@ var mongodb = require("mongodb"),
     mongoserver = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT, true),
     db_connector = new mongodb.Db('DevThis', mongoserver, 5);
 
-serialPort.on("open", function () {
-	console.log('open');
+db_connector.open(function(err, db) {
 
-	var buff = "";
-	db_connector.open(function(err, db) {
+	var collection = db.collection('RoadData');
 
+	serialPort.on("open", function () {
+		console.log('open');
+
+		var buff = "";
 		serialPort.on('data', function(data) {
 			buff += data;
 
 			if(String(data).indexOf('}') > -1){
 				
-				var collection = db.collection('RoadData');
 				var verklaring = JSON.parse(buff.toString());
 				
 				// Add some geo sugar
@@ -29,10 +34,22 @@ serialPort.on("open", function () {
 
 				// BUFFEL. DAAGH.
 				buff = "";
-				
 			}
 		});
+	});
+	
+	app.get('/roaddata.json', function(req, res) {
 
+		collection.find().toArray(function(err, items){
+			console.log(find);
+			res.json(items);
+		});
 	});
 
+	app.get('/', function(req, res) {
+		res.type('html');
+		res.send(fs.readFileSync('index.html'));
+	});
+
+	app.listen(3000);
 });
